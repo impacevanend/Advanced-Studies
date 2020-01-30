5b7f522bb99f7f797f36f36f28ae164d4e685d02
impacevanend <54188811+impacevanend@users.noreply.github.com>
2020-01-29 22:55:26 -0500

    Servidor Node.js

---
+++ nodeBible/comportamientoNode.js
@@ -0,0 +1,44 @@
+/*
+
+*código js -> v8 -> código maquina
+
+console.log("NodeJs");
+programa c++ -> v8 ->Código máquina.
+
+v8 github
+
+TODO CLIENTE Y SERVIDOR:
+
+*Nodejs -> tecnología de servidor(se escribe código js en el servidor)
+
+Servidor-> ordenador que presta servicios.
+cliente -> solicita los servicios del servidor.
+
+todo        -> Request -> 
+*cliente                   servidor
+todo        <- Response <-
+
+todo        -> Request -> 
+*Navegador        HTTP(FORMATO ESTÁNDAR DE COMUNICACIÓN)           Web Server
+todo        <- Response <-
+
+*Ajax -> técnica utilizada para la comunicación
+asíncrona entre cliente y servidor
+
+Todo JavaScript en el servidor:
+
+*Características que debe tener js para manejar un servidor.
+
+Archivos-> crear, borrar, leer y actualizar
+Bases de datos ->  utilizar una base de datos.
+Internet -> comunicación a traves de internet.
+Solicitud y respuesta ->
+Hilos-> manejar diferentes procesos.
+Hardware -> Manipular el hardware.
+Recursos -> Manipular los recursos del servidor.
+Software -> Comunicarse con otro software.
+
+
+
+
+*/ 
